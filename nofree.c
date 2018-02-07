c5d7e13b7763f29e98c2132820c32f4bceff6f78
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2018-02-06 18:53:08 -0700

    Initial commit and completed null.c and nofree.c

---
+++ nofree.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int *myInt;
+    myInt = (int *) malloc(1024);
+
+    return 0;
+}
---
+++ null.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int * myInt;
+    *myInt = NULL;
+    *myInt = 0;
+
+    return 0;
+}
